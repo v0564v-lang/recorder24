856a76b0c3d83d401fce74e27d7121199aa64275
v0564v-lang <v0564v@gmail.com>
2025-12-15 12:35:35 +0900

    Add MainActivity class for Recorder 24h app

---
+++ app/src/main/java/com/example/recorder24h/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.recorder24h
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
